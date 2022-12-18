446e8809ec535cd3c3b6f6ce3c6eb0b21efe3a6f
doggo <epicgamer101010101@gmail.com>
2022-12-17 20:29:54 -0600

    added some files

---
+++ Src/Utils/ProcessErrorHandling.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+Process.GetCurrentProcess().EnableRaisingEvents = true;
+
+Process.GetCurrentProcess().Exited += (sender, args) =>
+{
+    int exitCode = Process.GetCurrentProcess().ExitCode;
+    Console.WriteLine($"Process exited with code: {exitCode} time : {DateTime.Now}");
+};
+
+Process.GetCurrentProcess().Exited += (sender, args) =>
+{
+    int exitCode = Process.GetCurrentProcess().ExitCode;
+    Console.WriteLine($"Process will exit with code: {exitCode} time : {DateTime.Now}");
+};
+
+AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+{
+    Exception ex = (Exception)args.ExceptionObject;
+    Console.WriteLine($"Uncaught Exception: {ex.Message} time : {DateTime.Now}");
+    Console.Error.WriteLine(ex.StackTrace);
+    Environment.Exit(1);
+};
+
+TaskScheduler.UnobservedTaskException += (sender, args) =>
+{
+    Exception ex = args.Exception;
+    Console.WriteLine($"Unhandled rejection at {args.Observed} reason: {ex.Message} at {DateTime.Now}");
+    Console.Error.WriteLine(ex.StackTrace);
+    Environment.Exit(1);
+};
+
+CancellationTokenSource cts = new();
+
+Console.CancelKeyPress += (sender, args) =>
+{
+    Console.WriteLine($"Process {Environment.ProcessId} has been interrupted time : {DateTime.Now}");
+    args.Cancel = true;
+    cts.Cancel();
+};
+
+using (cts.Token.Register(() =>
+{
+    Console.WriteLine($"Process {Environment.ProcessId} received a SIGTERM signal time : {DateTime.Now}");
+    Environment.Exit(0);
+}))
+{
+    // empty...
+}
