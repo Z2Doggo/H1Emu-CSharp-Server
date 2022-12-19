b8c36641dfe52c0ae9286c12056687469f18eb2a
doggo <epicgamer101010101@gmail.com>
2022-12-19 13:29:02 -0600

    Update README.md, and add Globals to CS_Modules(WiP)

---
+++ CS_Modules/Types/CS/Globals.cs
@@ -0,0 +1,4 @@
+﻿namespace CS_Modules.Globals
+{
+    // TODO
+}
--- Program.cs
+++ Program.cs
@@ -11,26 +11,20 @@ namespace UDPServer
         {
             IPEndPoint endPoint = new(IPAddress.Parse("127.0.0.1"), 1115);
 
-            // Create a new UDP socket
             using Socket socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            // Bind the socket to the endpoint
+
             socket.Bind(endPoint);
 
-            // Create a buffer to store incoming data
-            byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[512];
 
-            // Create an EndPoint object to store the client's endpoint information
             EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
-            // Start listening for incoming connections
             while (true)
             {
                 try
                 {
-                    // Receive data from the client
                     int bytesReceived = socket.ReceiveFrom(buffer, ref clientEndPoint);
 
-                    // Convert the data to a string and print it to the console
                     string data = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
                     Console.WriteLine("Received: {0}", data);
                 }
