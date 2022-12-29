9d00619768575c3f8fcaccb7797b2804e439f239
doggo <epicgamer101010101@gmail.com>
2022-12-29 14:18:13 -0600

    SoeServer WiP..., prio for now...

--- Src/Servers/SoeServer/SoeServer.cs
+++ Src/Servers/SoeServer/SoeServer.cs
@@ -1,50 +1,67 @@
 ﻿namespace Servers.SoeServer
 {
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
     using Events;
     using H1EmuCore;
-    using SOEClient;
     using LogicalPacket;
     using SharedTypes;
-    using System.Threading;
-
+    using SOEClient;
     public class SOEServer : EventEmitter
     {
-        int _ServerPort { get; set; }
-        byte[] _CryptoKey { get; set; }
-        SoeProtocol _Protocol { get; set; }
-        int _UdpLength { get; set; } = 512;
-        bool _UseEncryption { get; set; } = true;
-        private Dictionary<string, SOEClient> _Clients = new Dictionary<string, SOEClient>();
-        private Thread _Connection;
-        int _CrcSeed { get; set; } = 0;
+        private readonly int _serverPort;
+        private readonly byte[] _cryptoKey;
+        private readonly SoeProtocol _protocol;
+        private readonly int _udpLength = 512;
+        private readonly bool _useEncryption = true;
+        private readonly Dictionary<string, SOEClient> _clients = new Dictionary<string, SOEClient>();
+        private readonly Thread _workerThread;
+        private readonly int _crcSeed = 0;
         public enum CrcLengthOptions // TEMPORARY WORKAROUND...
         {
             Zero = 0,
             Two = 2,
         }
-        CrcLengthOptions _CrcLength = CrcLengthOptions.Two;
-        int _WaitQueueTimeMs { get; set; } = 50;
-        int _PingTimeoutTime { get; set; } = 60000;
-        bool _UsePingTimeout { get; set; } = false;
-        private int _MaxMultiBufferSize { get; set; }
-        private readonly Timer _SoeClientRoutineLoopMethod;
-        private int _ResendTimeout { get; set; } = 300;
-        int PacketRatePerClient { get; set; } = 500;
-        private int _AckTiming { get; set; } = 80;
-        private int _RoutineTiming { get; set; } = 3;
-        bool _AllowedRawDataReception { get; set; } = true;
+        private readonly CrcLengthOptions _crcLength = CrcLengthOptions.Two;
+        private readonly int _waitQueueTimeMs = 50;
+        private readonly int _pingTimeoutTime = 60000;
+        private readonly bool _usePingTimeout = false;
+        private readonly int _maxMultiBufferSize;
+        private readonly Action<Action, int> _soeClientRoutineLoopMethod;
+        private readonly int _resendTimeout = 300;
+        private readonly int _packetRatePerClient = 500;
+        private readonly int _ackTiming = 80;
+        private readonly int _routineTiming = 3;
+        private readonly bool _allowedRawDataReception = true;
 
-        public SOEServer(int ServerPort, byte[] CryptoKey, bool DisableAntiDdos = false)
+        public SOEServer(int serverPort, byte[] cryptoKey, bool? disableAntiDdos = false) : base()
         {
-            _ServerPort = ServerPort;
-            _CryptoKey = CryptoKey;
-            _MaxMultiBufferSize = (int)(_UdpLength - 4 - _CrcLength);
-            _SoeClientRoutineLoopMethod = new Timer(ResetPacketsSent, null, 1000, Timeout.Infinite);
+            int bufferSize = 8192 * 4;
+            byte[] buffer = new byte[bufferSize];
+            _serverPort = serverPort;
+            _cryptoKey = cryptoKey;
+            _maxMultiBufferSize = (int)(_udpLength - 4 - _crcLength);
+            _workerThread = new Thread(() =>
+            {
+                // TODO: Replace this with the actual worker thread code
+            });
+            _workerThread.Start();
+
+            Timer timer = new Timer((state) =>
+            {
+                ResetPacketsSent();
+            }, null, 0, 1000); 
+            timer.Dispose();
         }
 
-        private void ResetPacketsSent(object State)
+        private void ResetPacketsSent()
         {
-            _SoeClientRoutineLoopMethod.Change(1000, Timeout.Infinite);
+            foreach (var client in _clients.Values)
+            {
+                client.PacketsSentThisSec = 0;
+            }
         }
     }
 }
